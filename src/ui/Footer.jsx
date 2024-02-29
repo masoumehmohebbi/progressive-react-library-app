2308a71d5ab6e9535456bfaa40e24b6b9231f9ec
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-02-29 22:25:28 +0330

    ✅ Updated Title

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -3,7 +3,7 @@ import { IoLibraryOutline } from "react-icons/io5";
 const Footer = () => {
   return (
     <footer className="bg-secondary-300 rounded-t-md p-5 mt-28">
-      <div className="grid grid-cols-4 items-start">
+      <div className="grid grid-cols-3 md:grid-cols-5 items-start">
         <div className="flex items-center gap-x-1 ml-5">
           <IoLibraryOutline className="w-9 h-9 text-slate-800" />
           <span className="font-bold text-slate-800">رابوک</span>
@@ -24,7 +24,8 @@ const Footer = () => {
         </ul>
       </div>
 
-      <hr className="my-7" />
+      <div className="my-7 bg-primary-300 h-[1px] w-full rounded-md"></div>
+
       <h5>رابوک 1402 © </h5>
     </footer>
   );
