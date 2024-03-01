a6f547f8617832f6991ec239a0b876d070ff77ff
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-03-01 14:09:28 +0330

    ✅ Added Suspense fallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Loading from "./ui/Loading.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
     </React.StrictMode>
   </BrowserRouter>
 );
