f9eeb01bace00867b99c3fec0dca5123af0764b4
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-03-23 14:17:33 +0330

    💄 Updated Jsx to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import Loading from './ui/Loading.jsx';
+import Loading from './ui/Loading';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <React.StrictMode>
       <Suspense fallback={<Loading />}>
